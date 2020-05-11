53dd4030bec8ee61eabc3c6cd679cf00fdc19152
hisaakikato <hisaakikato.work@gmail.com>
2020-05-11 22:21:02 +0900

    maltithread and export by ".npy"

--- pubmed_scraping.py
+++ pubmed_scraping.py
@@ -6,12 +6,12 @@ from concurrent.futures import ThreadPoolExecutor
 import time
 
 #################################################################
-APIkey = '&api_key=' + '--your api key--'
+APIkey = '&api_key=' + 'cfbe775651d713e0d6e21194021827922d07'
 keyword = "yeast"
 batch_max = 1000
 batch_size = 1000
 search_start = 0
-parallel = 4 #Up to 4, because API rate limit
+parallel = 10
 #################################################################
 
 EsearchURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term="
@@ -23,9 +23,11 @@ def get_soup(url):
     soup = BeautifulSoup(request.content, 'html.parser')
     return soup
 
-def get_ref_ids(url):
-    link_soup = get_soup(url)
+def get_data(id):
+    id = id.text
 
+    elink_url = ElinkURL + id + "&retmode=xml" + APIkey
+    link_soup = get_soup(elink_url)
     link_data = link_soup.find_all('linksetdb')
     references = [link.text.split('\n') for link in link_data if 'pubmed_pubmed_refs' in link.text]
 
@@ -34,11 +36,10 @@ def get_ref_ids(url):
     else:
         ref_ids = references[0][4::2]
 
-    return ref_ids
+    # time.sleep(0.05)
 
-def get_fetch_data(url):
-    fetch_soup = get_soup(url)
-
+    efetch_url = EfetchURL + id + "&retmode=xml" + APIkey
+    fetch_soup = get_soup(efetch_url)
     title = fetch_soup.find('articletitle').text
     pubdate = fetch_soup.find("daterevised").text.replace('\n', '')
     authors = ["{0} {1}".format(last.text,fore.text) for last,fore in zip(fetch_soup.find_all('lastname'), fetch_soup.find_all('forename'))]
@@ -47,9 +48,9 @@ def get_fetch_data(url):
     except AttributeError:
         abstract = 'None'
 
-    fetch_data = [title, pubdate, authors, abstract]
+    data = [id, ref_ids, title, pubdate, authors, abstract]
 
-    return fetch_data
+    return data
 
 
 def main():
@@ -57,33 +58,23 @@ def main():
     start = time.time()
 
     id_list = []
-    for retstart_num in range(search_start,batch_max,batch_size):
+    range_num = range(search_start,batch_max,batch_size)
+    for retstart_num in range_num:
         search_url = EsearchURL + keyword + '&retstart=' + str(retstart_num) + '&retmax=' + str(batch_size) + APIkey
         ids = get_soup(search_url).find_all('id')
         id_list.extend(ids)
-    id_list = [id.text for id in id_list]
 
     # print('id_list:' + str(time.time()-start))
     # step = time.time()
-    elink_urls = [ElinkURL + id + "&retmode=xml" + APIkey for id in id_list]
-    efetch_urls = [EfetchURL + id + "&retmode=xml" + APIkey for id in id_list]
-
-    article_data = []
+    
     with ThreadPoolExecutor(parallel) as executor:
-        ref_ids_results = list(executor.map(get_ref_ids, elink_urls))
-
-    with ThreadPoolExecutor(parallel) as executor:
-        fetch_data_results = list(executor.map(get_fetch_data, efetch_urls))
-
-    article_data.append([id_list, ref_ids_results, fetch_data_results])
-    article_data_arr = np.array(article_data[0]).T
+        results = list(executor.map(get_data, id_list))
+    results_arr = np.array(results)
 
     # print('article_data_list:' + str(time.time()-step))
     # step = time.time()
 
-    with open('article_data.csv', 'w', encoding='utf-8') as f:
-        writer = csv.writer(f, lineterminator="\n")
-        writer.writerows(article_data_arr)
+    np.save('article_data_.npy', results_arr)
 
     # print('write:' + str(time.time()-step))
     # print('total:' + str(time.time()-start))
